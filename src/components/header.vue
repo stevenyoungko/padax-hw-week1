dfead03f7f19f34cfd53e8e269bda511b43a7752
steven <stevenke60815@gmail.com>
2021-02-22 21:19:22 +0800

    [add] project init

---
+++ src/components/header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="nav">
+    <h3>My website</h3>
+    <div class="nav-list">
+      <div class="nav-item">Item1</div>
+      <div class="nav-item">Item1</div>
+      <div class="nav-item">Item1</div>
+      <div class="nav-item">Item1</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Header'
+  }
+</script>
+
+<style lang="sass" scoped>
+
+</style>

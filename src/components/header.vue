8297c2c1ead28322be0c92237381f1f5665b69c1
steven <stevenke60815@gmail.com>
2021-02-22 21:31:19 +0800

    [add] header css

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="nav">
-    <h3>My website</h3>
+    <h3 class="nav-title">My Website</h3>
     <div class="nav-list">
       <div class="nav-item">Item1</div>
-      <div class="nav-item">Item1</div>
-      <div class="nav-item">Item1</div>
-      <div class="nav-item">Item1</div>
+      <div class="nav-item">Item2</div>
+      <div class="nav-item">Item3</div>
+      <div class="nav-item">Item4</div>
     </div>
   </div>
 </template>

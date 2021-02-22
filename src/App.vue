de5bc6987212e79453c7c10788d272a151743e0a
steven <stevenke60815@gmail.com>
2021-02-22 22:04:24 +0800

    [add] main content

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,19 @@
     <div class="content">
       <h1 class="title">Welcome to MyHome</h1>
     </div>
+    <Main></Main>
   </div>
 </template>
 
 <script>
 
 import Header from './components/header'
+import Main from './components/main'
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Main
   }
 }
 </script>
---
+++ src/components/main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main-container">
+    <div class="list">
+      <div class="item" v-for="item in 8" :key="item">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <h3 class="title">{{ `Title${item}` }}</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Main'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

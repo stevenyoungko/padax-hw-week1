23cf101859bee3e82060d58df2b089de25100b80
steven <stevenke60815@gmail.com>
2021-03-10 23:34:22 +0800

    [add] get taipei data

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="main-container">
-    <div class="list">
-      <div class="item" v-for="item in 8" :key="item">
-        <img alt="Vue logo" src="../assets/logo.png">
-        <h3 class="title">{{ `Title${item}` }}</h3>
+    <div class="list"  v-if="dataList.length > 0">
+      <div class="item" v-for="item in dataList" :key="item._id">
+        <img alt="view" :src="getImage(item.file)" class="image">
+        <h3 class="title">{{ item.stitle }}</h3>
       </div>
     </div>
+    <div v-else>資料載入中...</div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Main'
+    name: 'Main',
+    data() {
+      return {
+        dataList: []
+      }
+    },
+    created() {
+      this.getTaipeiData()
+    },
+    methods: {
+      getTaipeiData() {
+        let url = 'https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json'
+        fetch(url)
+          .then(res => res.json())
+          .then((out) => {
+            this.dataList = out.result.results.slice(0,8)
+          })
+      },
+      getImage(url) {
+        return url.split('jpg')[0] + 'jpg'
+      }
+    }
   }
 </script>
 
